f752eb7ced5b471bdc828dd333bde9f0462ebe53
Taewan Kim <paladyn12@naver.com>
2024-11-29 20:08:11 +0900

    Commit

---
+++ src/Algorithm/implementation/Q25314.java
@@ -0,0 +1,22 @@
+package Algorithm.implementation;
+
+import java.util.Scanner;
+/**
+ * 문제 번호: 25314
+ * 문제 이름: 코딩은 체육과목 입니다
+ * 문제 링크: https://www.acmicpc.net/problem/
+ * 풀이: N이 4의 배수이므로 4로 나눠 long을 더할 횟수를 구함
+ */
+class Q25314 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        N /= 4;
+        String result = "";
+        for (int i = 0; i < N; i++) {
+            result += "long ";
+        }
+        result += "int";
+        System.out.print(result);
+    }
+}
