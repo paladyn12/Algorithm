a791fdd46958dd61c7a2d5bca1c136903c2799cc
Taewan Kim <paladyn12@naver.com>
2024-11-29 19:50:49 +0900

    Commit

--- src/Algorithm/implementation/Q11382.java
+++ src/Algorithm/implementation/Q11382.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
  * 문제 이름: 꼬마 정민
  * 문제 링크: https://www.acmicpc.net/problem/11382
  * 풀이: 입력 값이 int 범위를 넘어가므로 long 타입으로 입력받아 덧셈
-*/
+ */
 class Q11382 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
