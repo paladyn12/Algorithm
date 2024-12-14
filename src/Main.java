07dd9d81da4a89c8042aba183e020e03aabfa19d
Taewan Kim <paladyn12@naver.com>
2024-12-14 14:32:12 +0900

    Commit

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import java.io.*;
  * 문제 링크: https://www.acmicpc.net/problem/
  * 풀이:
  */
+
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
