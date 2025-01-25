31ccaf0ffa95d2b1020c982fe310e691a7547ced
Taewan Kim <paladyn12@naver.com>
2025-01-25 22:46:08 +0900

    풀었던 문제 재 풀이

--- src/Main.java
+++ src/Main.java
@@ -1,55 +1,36 @@
 import java.io.*;
-import java.util.ArrayDeque;
-import java.util.StringTokenizer;
+import java.lang.reflect.Array;
+import java.util.*;
 
 /**
- * 문제 번호: 14002
- * 문제 이름: 가장 긴 증가하는 부분 수열 4
- * 풀이: 부분 수열을 구하기 위해 stack 사용
+ * 문제 번호:
+ * 문제 이름:
+ * 풀이:
  */
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
         int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] nums = new int[N];
-        for (int i = 0; i < N; i++) nums[i] = Integer.parseInt(st.nextToken());
-        int[] dp = new int[N];
-        dp[0] = 1;
-        for (int i = 1; i < N; i++) {
-            dp[i] = 1;
-            for (int j = 0; j < i; j++) {
-                if (nums[j] < nums[i] && dp[i] < dp[j]+1) {
-                    dp[i] = dp[j] + 1;
+        StringBuilder sb = new StringBuilder();
+        if (N == 0) sb.append(0);
+        else {
+            while (N != 0) {
+                if (N % -2 == 0) {
+                    N /= -2;
+                    sb.append(0);
+                } else {
+                    N -= 1;
+                    N /= -2;
+                    sb.append(1);
                 }
             }
         }
+        sb.reverse();
 
-        int max = -1;
-        for (int i : dp) {
-            max = Math.max(i, max);
-        }
-
-        StringBuilder sb = new StringBuilder();
-        sb.append(max).append("\n");
-
-        int value = max;
-        ArrayDeque<Integer> stack = new ArrayDeque<>();
-        for (int i = N-1; i >= 0; i--) {
-            if (dp[i] == value) {
-                stack.addLast(nums[i]);
-                value--;
-            }
-        }
-
-        while (!stack.isEmpty()) {
-            sb.append(stack.pollLast()).append(" ");
-        }
-
-        bw.write(sb.toString());
-        bw.flush();
+        System.out.println(sb);
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         br.close();
         bw.close();
