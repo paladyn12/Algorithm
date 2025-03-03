0ff9f86df102de47733e7d11cff4c2388e866ae6
Taewan Kim <paladyn12@naver.com>
2025-03-03 20:04:57 +0900

    풀었던 문제 다시 풀이

--- src/Main.java
+++ src/Main.java
@@ -7,60 +7,26 @@ import java.util.*;
  * 풀이:
  */
 class Main {
+    static int[] array;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-        ArrayDeque<Character> stack = new ArrayDeque<>();
-        String str = br.readLine();
-
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < str.length(); i++) {
-            char c = str.charAt(i);
-            switch (c) {
-                case '+' :
-                case '-' :
-                case '*' :
-                case '/' :
-                    while (!stack.isEmpty() && priority(stack.peekLast()) >= priority(c)) {
-                        sb.append(stack.pollLast());
-                    }
-                    stack.addLast(c);
-                    break;
-                case '(' :
-                    stack.addLast('(');
-                    break;
-                case ')' :
-                    while (!stack.isEmpty() && stack.peekLast() != '(') {
-                        sb.append(stack.pollLast());
-                    }
-                    stack.pollLast();
-                    break;
-                default:
-                    sb.append(c);
-            }
-
+        int N = Integer.parseInt(br.readLine());
+        array = new int[N+1];
+        array[0] = 0;
+        array[1] = 0;
+        for (int i = 2; i <= N; i++) {
+            if (i % 6 == 0) {
+                array[i] = Math.min(array[i-1], Math.min(array[i/3], array[i/2])) + 1;
+            } else if (i % 3 == 0) {
+                array[i] = Math.min(array[i/3], array[i-1]) + 1;
+            } else if (i % 2 == 0) {
+                array[i] = Math.min(array[i/2], array[i-1]) + 1;
+            } else array[i] = array[i-1] + 1;
         }
 
-        while (!stack.isEmpty())
-            sb.append(stack.pollLast());
-
-        bw.write(sb.toString());
-        bw.flush();
+        System.out.println(array[N]);
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         br.close();
-        bw.close();
-    }
-    public static int priority(char operator){
-        if(operator=='(' || operator==')') {
-            return 0;
-        } else if (operator == '+' || operator == '-') {
-            return 1;
-        } else if (operator == '*' || operator == '/') {
-            return 2;
-        }
-        return -1;
     }
 }
