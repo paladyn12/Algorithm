c28e098a1ed3e3de2f048dc377d1efe8668a89e0
Taewan Kim <paladyn12@naver.com>
2025-03-07 18:29:45 +0900

    알고리즘 공부

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 /**
- * 문제 번호: 1010
- * 문제 이름: 다리 놓기
- * 풀이: mCn 구하기 >> dp 활용
+ * 문제 번호: 2225
+ * 문제 이름: 합분해
+ * 풀이: 점화식 dp[N][K] = dp[N][K-1] + dp[N-1][K]
  */
 class Main {
     public static void main(String[] args) throws IOException {
@@ -12,34 +12,27 @@ class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        int test_case = Integer.parseInt(br.readLine());
-        StringTokenizer st;
-        int N;
-        int M;
-        StringBuilder sb = new StringBuilder();
-        while (test_case-- > 0) {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
-            st = new StringTokenizer(br.readLine());
-            N = Integer.parseInt(st.nextToken());
-            M = Integer.parseInt(st.nextToken());
+        long[][] dp = new long[N+1][K+1];
 
-            long[][] dp = new long[M+1][M+1];
+        for (int i = 1; i <= N; i++) {
+            dp[i][1] = 1;
+        }
+        for (int i = 1; i <= K; i++) {
+            dp[0][i] = 1;
+        }
+        for (int i = 1; i <= N; i++) {
 
-            for (int i = 0; i <= M; i++) {
-                dp[i][0] = 1;
-                dp[i][i] = 1;
+            for (int j = 2; j <= K; j++) {
+                dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % 1000000000;
             }
-            for (int i = 2; i <= M; i++) {
-                for (int j = 1; j < i; j++) {
-                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
-                }
-            }
-
-            sb.append(dp[M][N]).append("\n");
         }
 
-        bw.write(sb.toString());
-        bw.flush();
+        System.out.println(dp[N][K]);
+
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         br.close();
         bw.close();
