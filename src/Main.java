0bc068d41c0aaa4414ad79bc6b8663ef689ed0c7
Taewan Kim <paladyn12@naver.com>
2025-03-09 14:38:09 +0900

    풀었던 문제 다시 풀이

--- src/Main.java
+++ src/Main.java
@@ -2,39 +2,32 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 /**
- * 문제 번호: 2225
- * 문제 이름: 합분해
- * 풀이: 점화식 dp[N][K] = dp[N][K-1] + dp[N-1][K]
+ * 문제 번호:
+ * 문제 이름:
+ * 풀이:
  */
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
+        int N = Integer.parseInt(br.readLine());
+        int[] pack = new int[N+1];
+        int[] dp = new int[N+1];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-
-        long[][] dp = new long[N+1][K+1];
-
         for (int i = 1; i <= N; i++) {
-            dp[i][1] = 1;
+            pack[i] = Integer.parseInt(st.nextToken());
+            dp[i] = pack[i];
         }
-        for (int i = 1; i <= K; i++) {
-            dp[0][i] = 1;
-        }
-        for (int i = 1; i <= N; i++) {
-
-            for (int j = 2; j <= K; j++) {
-                dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % 1000000000;
+        dp[0] = 0;
+        dp[1] = pack[1];
+        for (int i = 2; i <= N; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i-j] + pack[j]);
             }
         }
 
-        System.out.println(dp[N][K]);
-
+        System.out.println(dp[N]);
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         br.close();
-        bw.close();
     }
 }
