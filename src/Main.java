6d0050caeedef659c9e09076a21b3e3339f3937a
Taewan Kim <paladyn12@naver.com>
2025-03-16 01:25:37 +0900

    풀었던 문제 풀이

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,90 @@
 import java.io.*;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /**
- * 문제 번호:
- * 문제 이름:
- * 풀이:
+ * 문제 번호: 14500
+ * 문제 이름: 테트로미노
+ * 풀이: 가능한 모든 테트로미노의 합을 계산
  */
 class Main {
+    static int N;
+    static int M;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-        int[][] dp = new int[N + 1][K + 1];
-        Arrays.fill(dp[0], 1);
-        dp[0][0] = 0;
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= K; j++) {
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-                dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % 1000000000;
+        int[][] paper = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                paper[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        System.out.println(dp[N][K]);
+        int result = Integer.MIN_VALUE;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                result = Math.max(result, logic(paper, i, j));
+            }
+        }
+
+        System.out.println(result);
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         br.close();
         bw.close();
+    }
+    public static int logic(int[][] paper, int i, int j) {
+
+        int max = Integer.MIN_VALUE;
+        if (j+3 < M) max = Math.max(max, paper[i][j] + paper[i][j+1] + paper[i][j+2] + paper[i][j+3]);
+        if (i+3 < N) max = Math.max(max, paper[i][j] + paper[i+1][j] + paper[i+2][j] + paper[i+3][j]);
+        if (j + 1 < M && i+1 < N) max = Math.max(max, paper[i][j] + paper[i+1][j] + paper[i][j+1] + paper[i+1][j+1]);
+
+        if (j + 2 < M && i + 1 < N) max = Math.max(max, subLogic1(paper, i, j));
+        if (j + 1 < M && i + 2 < N) max = Math.max(max, subLogic2(paper, i, j));
+
+        return max;
+
+    }
+
+    private static int subLogic1(int[][] paper, int i, int j) {
+
+        int max = Integer.MIN_VALUE;
+
+        max = Math.max(max, paper[i][j] + paper[i][j+1] + paper[i][j+2] + paper[i+1][j+2]);
+        max = Math.max(max, paper[i][j] + paper[i][j+1] + paper[i+1][j+1] + paper[i+1][j+2]);
+        max = Math.max(max, paper[i][j] + paper[i][j+1] + paper[i+1][j+1] + paper[i][j+2]);
+        max = Math.max(max, paper[i][j] + paper[i+1][j] + paper[i+1][j+1] + paper[i+1][j+2]);
+        max = Math.max(max, paper[i+1][j] + paper[i][j+1] + paper[i+1][j+1] + paper[i+1][j+2]);
+        max = Math.max(max, paper[i+1][j] + paper[i+1][j+1] + paper[i+1][j+2] + paper[i][j+2]);
+        max = Math.max(max, paper[i][j] + paper[i+1][j] + paper[i+1][j+1] + paper[i+1][j+2]);
+        max = Math.max(max, paper[i+1][j] + paper[i+1][j+1] + paper[i][j+1] + paper[i][j+2]);
+
+
+
+        return max;
+    }
+
+    private static int subLogic2(int[][] paper, int i, int j) {
+
+        int max = Integer.MIN_VALUE;
+
+        max = Math.max(max, paper[i][j] + paper[i+1][j] + paper[i+2][j] + paper[i+2][j+1]);
+        max = Math.max(max, paper[i][j] + paper[i+1][j] + paper[i+1][j+1] + paper[i+2][j+1]);
+        max = Math.max(max, paper[i][j] + paper[i+1][j] + paper[i+1][j+1] + paper[i+2][j]);
+        max = Math.max(max, paper[i][j] + paper[i][j+1] + paper[i+1][j+1] + paper[i+2][j+1]);
+        max = Math.max(max, paper[i][j+1] + paper[i+1][j+1] + paper[i+1][j] + paper[i+2][j]);
+        max = Math.max(max, paper[i][j+1] + paper[i+1][j+1] + paper[i+2][j+1] + paper[i+2][j]);
+        max = Math.max(max, paper[i][j+1] + paper[i+1][j+1] + paper[i+1][j] + paper[i+2][j+1]);
+        max = Math.max(max, paper[i][j+1] + paper[i][j] + paper[i+1][j] + paper[i+2][j]);
+
+
+        return max;
     }
 }
