211ebaa004c3088edd3bc6689000b3ef97c746ef
Taewan Kim <paladyn12@naver.com>
2025-03-19 17:40:01 +0900

    풀었던 문제 풀이

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,16 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 /**
- * 문제 번호: 14500
- * 문제 이름: 테트로미노
- * 풀이: 가능한 모든 테트로미노의 합을 계산
+ * 문제 번호: 9663
+ * 문제 이름: N-Queen
+ * 풀이: 백트래킹 기법을 이용하여 매 호출마다 퀸을 놓지 못하는 위치를 업데이트
  */
 class Main {
+
+    static int[] position;
     static int N;
-    static int M;
+    static int count = 0;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -16,75 +19,40 @@ class Main {
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        position = new int[N];
 
-        int[][] paper = new int[N][M];
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) {
-                paper[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-
-        int result = Integer.MIN_VALUE;
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                result = Math.max(result, logic(paper, i, j));
-            }
-        }
-
-        System.out.println(result);
+        dfs(0);
+        System.out.println(count);
+        bw.flush();
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         br.close();
         bw.close();
     }
-    public static int logic(int[][] paper, int i, int j) {
 
-        int max = Integer.MIN_VALUE;
-        if (j+3 < M) max = Math.max(max, paper[i][j] + paper[i][j+1] + paper[i][j+2] + paper[i][j+3]);
-        if (i+3 < N) max = Math.max(max, paper[i][j] + paper[i+1][j] + paper[i+2][j] + paper[i+3][j]);
-        if (j + 1 < M && i+1 < N) max = Math.max(max, paper[i][j] + paper[i+1][j] + paper[i][j+1] + paper[i+1][j+1]);
+    public static void dfs(int depth) {
 
-        if (j + 2 < M && i + 1 < N) max = Math.max(max, subLogic1(paper, i, j));
-        if (j + 1 < M && i + 2 < N) max = Math.max(max, subLogic2(paper, i, j));
+        if (depth == N) {
+            count++;
+            return;
+        }
 
-        return max;
+        for (int i = 0; i < N; i++) {
+            position[depth] = i;
+            if (possibility(depth)) {
+                dfs(depth + 1);
+            }
 
+        }
     }
 
-    private static int subLogic1(int[][] paper, int i, int j) {
+    private static boolean possibility(int depth) {
+        for (int i = 0; i < depth; i++) {
 
-        int max = Integer.MIN_VALUE;
+            if (position[i] == position[depth]) return false;
+            if (Math.abs(depth - i) == Math.abs(position[depth] - position[i])) return false;
 
-        max = Math.max(max, paper[i][j] + paper[i][j+1] + paper[i][j+2] + paper[i+1][j+2]);
-        max = Math.max(max, paper[i][j] + paper[i][j+1] + paper[i+1][j+1] + paper[i+1][j+2]);
-        max = Math.max(max, paper[i][j] + paper[i][j+1] + paper[i+1][j+1] + paper[i][j+2]);
-        max = Math.max(max, paper[i][j] + paper[i+1][j] + paper[i+1][j+1] + paper[i+1][j+2]);
-        max = Math.max(max, paper[i+1][j] + paper[i][j+1] + paper[i+1][j+1] + paper[i+1][j+2]);
-        max = Math.max(max, paper[i+1][j] + paper[i+1][j+1] + paper[i+1][j+2] + paper[i][j+2]);
-        max = Math.max(max, paper[i][j] + paper[i+1][j] + paper[i+1][j+1] + paper[i+1][j+2]);
-        max = Math.max(max, paper[i+1][j] + paper[i+1][j+1] + paper[i][j+1] + paper[i][j+2]);
-
-
-
-        return max;
+        }
+        return true;
     }
 
-    private static int subLogic2(int[][] paper, int i, int j) {
-
-        int max = Integer.MIN_VALUE;
-
-        max = Math.max(max, paper[i][j] + paper[i+1][j] + paper[i+2][j] + paper[i+2][j+1]);
-        max = Math.max(max, paper[i][j] + paper[i+1][j] + paper[i+1][j+1] + paper[i+2][j+1]);
-        max = Math.max(max, paper[i][j] + paper[i+1][j] + paper[i+1][j+1] + paper[i+2][j]);
-        max = Math.max(max, paper[i][j] + paper[i][j+1] + paper[i+1][j+1] + paper[i+2][j+1]);
-        max = Math.max(max, paper[i][j+1] + paper[i+1][j+1] + paper[i+1][j] + paper[i+2][j]);
-        max = Math.max(max, paper[i][j+1] + paper[i+1][j+1] + paper[i+2][j+1] + paper[i+2][j]);
-        max = Math.max(max, paper[i][j+1] + paper[i+1][j+1] + paper[i+1][j] + paper[i+2][j+1]);
-        max = Math.max(max, paper[i][j+1] + paper[i][j] + paper[i+1][j] + paper[i+2][j]);
-
-
-        return max;
-    }
 }
