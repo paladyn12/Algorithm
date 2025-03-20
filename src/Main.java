49f676035f070c0f03313a12b113d031fe063999
Taewan Kim <paladyn12@naver.com>
2025-03-20 16:13:10 +0900

    풀었던 문제 풀이

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,11 @@ import java.util.StringTokenizer;
  * 풀이: 백트래킹 기법을 이용하여 매 호출마다 퀸을 놓지 못하는 위치를 업데이트
  */
 class Main {
+
     static boolean[] visit;
     static int[] arr;
+    static int N;
+    static int M;
     static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
@@ -17,11 +20,11 @@ class Main {
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
         visit = new boolean[N];
-        int M = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
         arr = new int[M];
-        dfs(N, M, 0);
+        dfs(0);
 
         bw.write(sb.toString());
         bw.flush();
@@ -30,7 +33,7 @@ class Main {
         bw.close();
     }
 
-    public static void dfs(int N, int M, int depth) {
+    public static void dfs(int depth) {
 
         // 재귀 깊이가 M과 같아지면 탐색 과정에서 담았던 배열 출력
         if (depth == M) {
@@ -45,9 +48,8 @@ class Main {
 
             // 만약 해당 노드(값)을 방문하지 않았으면
             if (visit[i] == false) {
-                visit[i] = true;      // 해당 노드를 방문 상태로 변경
                 arr[depth] = i + 1;   // 해당 깊이를 index로 하여 i + 1 값 저장
-                dfs(N, M, depth + 1);  // 다음 자식 노드 방문을 위해 depth를 1 증가시키며 재귀호출
+                dfs(depth + 1);  // 다음 자식 노드 방문을 위해 depth를 1 증가시키며 재귀호출
 
                 // 자식노드 방문이 끝나고 돌아오면 방문노드를 방문하지 않은 상태로 변경
                 visit[i] = false;
