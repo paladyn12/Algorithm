592f62ad0f57e6b6dedf0b521c585161415c6c39
Taewan Kim <paladyn12@naver.com>
2025-03-21 17:40:34 +0900

    알고리즘 공부

---
+++ src/BAEKJOON/백트래킹/Q15657_백트래킹.java
@@ -0,0 +1,69 @@
+package BAEKJOON.백트래킹;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ * 문제 번호: 15657
+ * 문제 이름: N과 M (8)
+ * 풀이: 백트래킹 기본 문제
+ */
+class Q15657_백트래킹 {
+
+    static int N;
+    static int M;
+    static int[] array;
+    static int[] nums;
+    static boolean[] visit;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        array = new int[M];
+        nums = new int[N];
+        visit = new boolean[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(nums);
+
+        dfs(0, 0);
+
+        bw.write(sb.toString());
+        bw.flush();
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        br.close();
+        bw.close();
+    }
+
+    public static void dfs(int depth, int start) {
+
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(array[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = start; i < N; i++) {
+            array[depth] = nums[i];
+            dfs(depth + 1, i);
+//            if (!visit[i]) {
+//                array[depth] = nums[i];
+//                visit[i] = true;
+//                dfs(depth + 1);
+//            }
+//            visit[i] = false;
+        }
+    }
+}
