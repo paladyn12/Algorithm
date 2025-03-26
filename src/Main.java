fccc6c2712ff92f1b0692a568385f210e3e8c072
Taewan Kim <paladyn12@naver.com>
2025-03-26 17:15:26 +0900

    풀었던 문제 다시 풀이

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.util.Arrays;
 import java.util.StringTokenizer;
+
 /**
  * 문제 번호:
  * 문제 이름:
@@ -8,32 +9,36 @@ import java.util.StringTokenizer;
  */
 class Main {
 
-    static int N;
-    static int M;
-    static int[] array;
-    static int[] nums;
-    static boolean[] visit;
-    static StringBuilder sb = new StringBuilder();
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        array = new int[M];
-        nums = new int[N];
-        visit = new boolean[N];
+        int test_case = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        int N;
+        int M;
+        StringBuilder sb = new StringBuilder();
+        while (test_case-- > 0) {
 
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            nums[i] = Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+
+            long[][] dp = new long[M+1][M+1];
+
+            for (int i = 0; i <= M; i++) {
+                dp[i][0] = 1;
+                dp[i][i] = 1;
+            }
+            for (int i = 2; i <= M; i++) {
+                for (int j = 1; j < i; j++) {
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                }
+            }
+
+            sb.append(dp[M][N]).append("\n");
         }
-        Arrays.sort(nums);
-
-        dfs(0);
 
         bw.write(sb.toString());
         bw.flush();
@@ -42,25 +47,4 @@ class Main {
         bw.close();
     }
 
-    public static void dfs(int depth) {
-
-        if (depth == M) {
-            for (int i = 0; i < M; i++) {
-                sb.append(array[i]).append(" ");
-            }
-            sb.append("\n");
-            return;
-        }
-
-        for (int i = 0; i < N; i++) {
-            array[depth] = nums[i];
-            dfs(depth + 1);
-//            if (!visit[i]) {
-//                array[depth] = nums[i];
-//                visit[i] = true;
-//                dfs(depth + 1);
-//            }
-//            visit[i] = false;
-        }
-    }
 }
