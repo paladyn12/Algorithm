bf3e6434ef33a6a5e1720574e327eb7bbe95f996
Taewan Kim <paladyn12@naver.com>
2025-03-28 15:59:12 +0900

    풀었던 문제 다시 풀이

--- src/Main.java
+++ src/Main.java
@@ -11,22 +11,29 @@ class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        int n = Integer.parseInt(br.readLine());
+        int index = 0;
 
-        int N = Integer.parseInt(br.readLine());
-        int[][] dp = new int[N+1][3];
-        dp[1] = new int[]{1,1,1};
-        for (int i = 2; i <= N; i++) {
-            dp[i][0] = (dp[i-1][0] + dp[i-1][1] + dp[i-1][2]) % 9901;
-            dp[i][1] = (dp[i-1][0] + dp[i-1][2]) % 9901;
-            dp[i][2] = (dp[i-1][0] + dp[i-1][1]) % 9901;
+        for (int i = 666; true; i++) {
+            int count = 0;
+            int I = i;
+            while (I > 0) {
+                if (I % 10 == 6) count++;
+                else count = 0;
+                I /= 10;
+                if (count == 3) {
+                    index++;
+                    break;
+                }
+            }
+            if (index == n) {
+                System.out.println(i);
+                break;
+            }
         }
-
-        System.out.println((dp[N][0] + dp[N][1] + dp[N][2]) % 9901);
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         br.close();
-        bw.close();
     }
 
 }
