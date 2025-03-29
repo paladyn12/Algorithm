702a5b137cdd46324fd3f9e08b851578a297e736
Taewan Kim <paladyn12@naver.com>
2025-03-29 20:25:44 +0900

    풀었던 문제 다시 풀이

--- src/Main.java
+++ src/Main.java
@@ -11,29 +11,29 @@ class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        int n = Integer.parseInt(br.readLine());
-        int index = 0;
 
-        for (int i = 666; true; i++) {
-            int count = 0;
-            int I = i;
-            while (I > 0) {
-                if (I % 10 == 6) count++;
-                else count = 0;
-                I /= 10;
-                if (count == 3) {
-                    index++;
-                    break;
-                }
-            }
-            if (index == n) {
-                System.out.println(i);
-                break;
-            }
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n];
+        int[] array = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            array[i] = Integer.parseInt(st.nextToken());
         }
+        dp[0] = array[0];
+        for (int i = 1; i < n; i++) {
+            dp[i] = Math.max(array[i], dp[i-1] + array[i]);
+        }
+        int max = Integer.MIN_VALUE;
+        for (int i : dp) {
+            if (i > max) max = i;
+        }
+
+        System.out.println(max);
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         br.close();
+        bw.close();
     }
 
 }
