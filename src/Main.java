195c440cfe5d67d308108a7ddf2ea91a1391809d
Taewan Kim <paladyn12@naver.com>
2025-04-01 14:29:31 +0900

    풀었던 문제 다시 풀이

--- src/Main.java
+++ src/Main.java
@@ -8,28 +8,50 @@ import java.util.StringTokenizer;
  * 풀이:
  */
 class Main {
+    static int[] position;
+    static int N;
+    static int count = 0;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        int X = Integer.parseInt(br.readLine());
-        int line = 1;
-        int sum = 1;
-        while (X > sum) {
-            line++;
-            sum += line;
-        }
-        int position = X - sum + line;
 
-        StringBuilder sb = new StringBuilder();
-        if ((line%2)==1) {
-            sb.append(line - position + 1).append("/").append(position);
-        } else sb.append(position).append("/").append(line - position + 1);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        position = new int[N];
 
-        bw.write(sb.toString());
+        dfs(0);
+        System.out.println(count);
         bw.flush();
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         br.close();
         bw.close();
+    }
+
+    public static void dfs(int depth) {
+
+        if (depth == N) {
+            count++;
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            position[depth] = i;
+            if (possibility(depth)) {
+                dfs(depth + 1);
+            }
+
+        }
+    }
+
+    private static boolean possibility(int depth) {
+        for (int i = 0; i < depth; i++) {
+
+            if (position[i] == position[depth]) return false;
+            if (Math.abs(depth - i) == Math.abs(position[depth] - position[i])) return false;
+
+        }
+        return true;
     }
 }
