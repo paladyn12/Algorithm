e7bb76a06505c5a7fac1aa278634f09e24182fb2
Taewan Kim <paladyn12@naver.com>
2025-04-13 22:41:17 +0900

    알고리즘 공부

--- src/Main.java
+++ src/Main.java
@@ -12,49 +12,40 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        ArrayList<Integer> stack = new ArrayList();
-        int size = 0;
-        int top = -1;
-        int N = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < N; i++) {
-            String[] split = br.readLine().split(" ");
-            if (split.length == 2) {
-                stack.add(Integer.parseInt(split[1]));
-                size++;
-                top++;
-            } else {
-                String operation = split[0];
-                switch (operation) {
-                    case "pop":
-                        if (size >= 1) {
-                            size--;
-                            top--;
-                            sb.append(stack.remove(0)).append("\n");
-                        }
-                        else sb.append(-1).append("\n");
-                        break;
-                    case "size":
-                        sb.append(size).append("\n");
-                        break;
-                    case "empty":
-                        if (size == 0) sb.append(1).append("\n");
-                        else sb.append(0).append("\n");
-                        break;
-                    case "front":
-                        if(top >= 0) sb.append(stack.get(0)).append("\n");
-                        else sb.append(-1).append("\n");
-                        break;
-                    case "back":
-                        if(top >= 0) sb.append(stack.get(top)).append("\n");
-                        else sb.append(-1).append("\n");
-                        break;
+        int N = Integer.parseInt(br.readLine());
+        int[][] price = new int[N+1][3];
+        StringTokenizer st;
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            price[i][0] = Integer.parseInt(st.nextToken());
+            price[i][1] = Integer.parseInt(st.nextToken());
+            price[i][2] = Integer.parseInt(st.nextToken());
+        }
+        int[][] dp = new int[N+1][3];
+        int max = 1000 * 1000;
+        int answer = 1000 * 1000;
+        for (int color = 0; color < 3; color++) {
+
+            for (int i = 0; i < 3; i++) {
+                if (i == color) dp[1][i] = price[1][i];
+                else dp[1][i] = max;
+            }
+
+            for (int i = 2; i <= N; i++) {
+                dp[i][0] = Math.min(dp[i-1][1], dp[i-1][2]) + price[i][0];
+                dp[i][1] = Math.min(dp[i-1][0], dp[i-1][2]) + price[i][1];
+                dp[i][2] = Math.min(dp[i-1][0], dp[i-1][1]) + price[i][2];
+            }
+
+            for (int i = 0; i < 3; i++) {
+                if (i != color) {
+                    answer = Math.min(answer, dp[N][i]);
                 }
             }
         }
-        bw.write(sb.toString());
-        bw.flush();
+
+        System.out.println(answer);
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         br.close();
         bw.close();
