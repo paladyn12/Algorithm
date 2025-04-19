35daecd370ccd1306f03a23e1e0ad0542edc6543
Taewan Kim <paladyn12@naver.com>
2025-04-19 15:04:00 +0900

    알고리즘 공부

--- src/Main.java
+++ src/Main.java
@@ -2,41 +2,28 @@ import java.io.*;
 import java.util.*;
 
 /**
- * 문제 번호: 1931
- * 문제 이름: 회의실 배정
- * 풀이: 회의가 끝나는 시간을 기준으로 Sort
- * 각 회의의 시작 시간이 저장했던 endTime 보다 크거나 같으면 (이전 회의가 끝난 이후의 회의이면)
- * 그 회의를 시작하는 것으로 가정하여 endTime을 그 회의의 종료 시간으로 초기화
+ * 문제 번호:
+ * 문제 이름:
+ * 풀이:
  */
 class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        int N = Integer.parseInt(br.readLine());
-        int[][] info = new int[N][2];
-        StringTokenizer st;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] A = new int[N];
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            info[i][0] = Integer.parseInt(st.nextToken());
-            info[i][1] = Integer.parseInt(st.nextToken());
+            A[i] = Integer.parseInt(br.readLine());
         }
 
-        Arrays.sort(info, (t1, t2) -> {
-            if (t1[1] == t2[1]) return t1[0] - t2[0];
-            return t1[1] - t2[1];
-        });
-
-        int endTime = 0;
         int count = 0;
-
-        for (int i = 0; i < N; i++) {
-            if (info[i][0] >= endTime) {
-                count++;
-                endTime = info[i][1];
-            }
+        for (int i = N-1; i >= 0; i--) {
+            count += K / A[i];
+            K %= A[i];
         }
 
         System.out.println(count);
