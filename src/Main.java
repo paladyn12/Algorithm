b167c42dd23030ee0dcffcb37148df51625c74fa
Taewan Kim <paladyn12@naver.com>
2025-05-10 15:31:34 +0900

    알고리즘 공부

--- src/Main.java
+++ src/Main.java
@@ -10,40 +10,36 @@ import java.util.StringTokenizer;
  * 풀이:
  */
 class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        int test_case = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        int[][] info = new int[N][2];
         StringTokenizer st;
-        int N;
-        int M;
-        StringBuilder sb = new StringBuilder();
-        while (test_case-- > 0) {
-
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            N = Integer.parseInt(st.nextToken());
-            M = Integer.parseInt(st.nextToken());
-
-            long[][] dp = new long[M+1][M+1];
-
-            for (int i = 0; i <= M; i++) {
-                dp[i][0] = 1;
-                dp[i][i] = 1;
-            }
-            for (int i = 2; i <= M; i++) {
-                for (int j = 1; j < i; j++) {
-                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
-                }
-            }
-
-            sb.append(dp[M][N]).append("\n");
+            info[i][0] = Integer.parseInt(st.nextToken());
+            info[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        bw.write(sb.toString());
-        bw.flush();
+        Arrays.sort(info, (t1, t2) -> {
+            if (t1[1] == t2[1]) return t1[0] - t2[0];
+            return t1[1] - t2[1];
+        });
+
+        int endTime = 0;
+        int count = 0;
+
+        for (int i = 0; i < N; i++) {
+            if (info[i][0] >= endTime) {
+                count++;
+                endTime = info[i][1];
+            }
+        }
+
+        System.out.println(count);
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         br.close();
         bw.close();
