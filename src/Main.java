5ed43938d1d6bb07a9826d6834dd06850abfbac7
Taewan Kim <paladyn12@naver.com>
2025-05-28 13:52:40 +0900

    알고리즘 공부

--- src/Main.java
+++ src/Main.java
@@ -2,109 +2,38 @@ import java.io.*;
 import java.util.*;
 
 /**
- * 문제 번호: 16929
- * 문제 이름: Two Dots
- * 풀이 : bfs에 탐색하려는 값을 넣음
- * 탐색한 depth 기록
- * 만약 탐색하는 노드의 depth가 nextDepth이면 (같은 depth의 노드) 순환
+ * 문제 번호:
+ * 문제 이름:
+ * 풀이 :
  */
 
 class Main {
-
-    static int N;
-    static int M;
-    static int[][] visit;
-
-    static char[][] map;
-    static int[] dr = new int[]{-1, 0, 1, 0};
-    static int[] dc = new int[]{0, 1, 0, -1};
-
-    static class Point {
-        int r;
-        int c;
-        int depth;
-
-        public Point(int r, int c) {
-            this.r = r;
-            this.c = c;
-            depth = 1;
-        }
-
-        public Point(int r, int c, int depth) {
-            this.r = r;
-            this.c = c;
-            this.depth = depth;
-        }
-    }
-
-    static StringBuilder sb = new StringBuilder();
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        visit = new int[N][M];
-        map = new char[N][M];
-        for (int i = 0; i < N; i++) {
-            String input = br.readLine();
-            for (int j = 0; j < M; j++) {
-                map[i][j] = input.charAt(j);
-            }
+        int test_case = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        long[] dp = new long[1000001];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+        for (int i = 4; i <= 1000000; i++) {
+            dp[i] = (dp[i-3] + dp[i-2] + dp[i-1]) % 1000000009;
         }
 
-        boolean success = false;
+        while (test_case-- > 0) {
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (visit[i][j] == 0) {
-                    visit[i][j] = 1;
-                    if (bfs(i, j, map[i][j])) success = true;
-                }
-                if (success) break;
-            }
-            if (success) break;
+            int n = Integer.parseInt(br.readLine());
+            sb.append(dp[n]).append("\n");
         }
-
-        String result = success ? "Yes" : "No";
-        sb.append(result);
 
         bw.write(sb.toString());
         bw.flush();
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         br.close();
-        bw.close();
     }
 
-    static boolean bfs(int r, int c, char alphabet) {
-
-        ArrayDeque<Point> queue = new ArrayDeque<>();
-        queue.add(new Point(r, c));
-
-        while (!queue.isEmpty()) {
-            Point cur = queue.removeFirst();
-            int curR = cur.r;
-            int curC = cur.c;
-            int curDepth = cur.depth;
-
-            for (int i = 0; i < 4; i++) {
-                int nextR = curR + dr[i];
-                int nextC = curC + dc[i];
-                int nextDepth = curDepth + 1;
-                if (nextR < 0 || nextR >= N || nextC < 0 || nextC >= M) continue;
-                if (map[nextR][nextC] == alphabet) {
-                    if (visit[nextR][nextC] == 0) {
-                        visit[nextR][nextC] = nextDepth;
-                        queue.addLast(new Point(nextR, nextC, nextDepth));
-                    } else if (visit[nextR][nextC] == nextDepth){
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
-    }
 }
