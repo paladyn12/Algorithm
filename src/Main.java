f654e81ef643f48693dcdde7935bde2de1a8f4de
Taewan Kim <paladyn12@naver.com>
2025-06-02 00:43:09 +0900

    알고리즘 공부

--- src/Main.java
+++ src/Main.java
@@ -13,25 +13,23 @@ class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        int N = Integer.parseInt(br.readLine());
-        int[][] price = new int[N + 1][3];
-        StringTokenizer st;
-        for (int i = 1; i <= N; i++) {
-            st = new StringTokenizer(br.readLine());
-            price[i][0] = Integer.parseInt(st.nextToken());
-            price[i][1] = Integer.parseInt(st.nextToken());
-            price[i][2] = Integer.parseInt(st.nextToken());
-        }
-        int[][] dp = new int[N + 1][3];
-        dp[1] = new int[]{price[1][0], price[1][1], price[1][2]};
-        for (int i = 2; i <= N; i++) {
-            dp[i][0] = Math.min(dp[i-1][1], dp[i-1][2]) + price[i][0];
-            dp[i][1] = Math.min(dp[i-1][0], dp[i-1][2]) + price[i][1];
-            dp[i][2] = Math.min(dp[i-1][0], dp[i-1][1]) + price[i][2];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] A = new int[N];
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(br.readLine());
         }
 
-        System.out.println(Math.min(Math.min(dp[N][0], dp[N][1]), dp[N][2]));
+        int count = 0;
+        for (int i = N-1; i >= 0; i--) {
+            count += K / A[i];
+            K %= A[i];
+        }
+
+        System.out.println(count);
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         br.close();
+        bw.close();
     }
 }
