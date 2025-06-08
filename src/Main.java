03b2eb8103a46a8440c6097727187bc498f2ee31
Taewan Kim <paladyn12@naver.com>
2025-06-09 01:24:08 +0900

    알고리즘 공부

--- src/Main.java
+++ src/Main.java
@@ -8,37 +8,43 @@ import java.util.*;
  */
 
 class Main {
+    static boolean[] visit;
+    static int[] nums;
+
+    static int S;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        int N = Integer.parseInt(br.readLine());
-        int[][] info = new int[N][2];
-        StringTokenizer st;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
+        nums = new int[N];
+        visit = new boolean[N];
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            info[i][0] = Integer.parseInt(st.nextToken());
-            info[i][1] = Integer.parseInt(st.nextToken());
+            nums[i] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(info, (t1, t2) -> {
-            if (t1[1] == t2[1]) return t1[0] - t2[0];
-            return t1[1] - t2[1];
-        });
-
-        int endTime = 0;
         int count = 0;
 
-        for (int i = 0; i < N; i++) {
-            if (info[i][0] >= endTime) {
-                count++;
-                endTime = info[i][1];
+        for (int bit = 1; bit < (1 << N); bit++) {
+
+            int sum = 0;
+            for (int i = 0; i < N; i++) {
+                if ((bit & (1 << i)) != 0) {
+                    sum += nums[i];
+                }
             }
+            if (sum == S) count++;
+
         }
 
         System.out.println(count);
+
+        bw.flush();
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         br.close();
         bw.close();
