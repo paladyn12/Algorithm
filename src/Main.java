851728daf84758c52978f6dcc086893f0895dc5c
Taewan Kim <paladyn12@naver.com>
2025-06-10 00:58:27 +0900

    알고리즘 공부

--- src/Main.java
+++ src/Main.java
@@ -8,42 +8,32 @@ import java.util.*;
  */
 
 class Main {
-    static boolean[] visit;
-    static int[] nums;
-
-    static int S;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        int N = Integer.parseInt(br.readLine());
+        int[][] dots = new int[N][2];
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        S = Integer.parseInt(st.nextToken());
-        nums = new int[N];
-        visit = new boolean[N];
-        st = new StringTokenizer(br.readLine());
+        StringTokenizer st;
         for (int i = 0; i < N; i++) {
-            nums[i] = Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(br.readLine());
+            dots[i][0] = Integer.parseInt(st.nextToken());
+            dots[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        int count = 0;
-
-        for (int bit = 1; bit < (1 << N); bit++) {
-
-            int sum = 0;
-            for (int i = 0; i < N; i++) {
-                if ((bit & (1 << i)) != 0) {
-                    sum += nums[i];
-                }
+        Arrays.sort(dots, (a,b) -> {
+            if (a[0] == b[0]) {
+                return Integer.compare(a[1], b[1]);
             }
-            if (sum == S) count++;
+            return Integer.compare(a[0], b[0]);
+        });
 
+        StringBuilder sb = new StringBuilder();
+        for (int[] dot : dots) {
+            sb.append(dot[0]).append(" ").append(dot[1]).append("\n");
         }
-
-        System.out.println(count);
-
+        bw.write(sb.toString());
         bw.flush();
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         br.close();
