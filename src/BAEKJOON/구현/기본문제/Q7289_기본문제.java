ddd5740189601fe2da2c7e641e6951031a48356e
Taewan Kim <81952405+paladyn12@users.noreply.github.com>
2025-08-05 10:54:37 +0900

    Create Q7289_기본문제.java

---
+++ src/BAEKJOON/구현/기본문제/Q7289_기본문제.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+
+class Q7289_기본문제 {
+    
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("296\npaladyn");
+    }
+}
