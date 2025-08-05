bf296302ddda413af25c4949ffa645364f4c48bb
Taewan Kim <81952405+paladyn12@users.noreply.github.com>
2025-08-05 13:09:28 +0900

    Create Q31403_기본문제.java

---
+++ src/BAEKJOON/구현/기본문제/Q31403_기본문제.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+class Q31403_기본문제 {
+    
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String A = br.readLine();
+        String B = br.readLine();
+        int C = Integer.parseInt(br.readLine());
+
+        System.out.println(Integer.parseInt(A) + Integer.parseInt(B) - C);
+        System.out.println(Integer.parseInt(A + B) - C);
+        
+    }
+}
