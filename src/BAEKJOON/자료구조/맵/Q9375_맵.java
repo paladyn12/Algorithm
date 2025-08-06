8adc8cfb742c8a94a38b2e4d09e0c0284a5e10ec
Taewan Kim <81952405+paladyn12@users.noreply.github.com>
2025-08-06 15:38:53 +0900

    Create Q9375_맵.java

---
+++ src/BAEKJOON/자료구조/맵/Q9375_맵.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+class Q9375_맵 {
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        HashMap<String, String> map = new HashMap<>();
+        for (int i = 0; i < N; i++) {
+            String[] input = br.readLine().split(" ");
+            map.put(input[0], input[1]);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < M; i++) {
+            sb.append(map.get(br.readLine())).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
