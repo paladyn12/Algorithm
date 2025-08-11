eb1152e109249314003ff92948caae263383d83e
Taewan Kim <81952405+paladyn12@users.noreply.github.com>
2025-08-11 15:23:38 +0900

    Create Q1927_이분탐색.java

---
+++ src/BAEKJOON/이분탐색/Q1927_이분탐색.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Q1927_이분탐색 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int K = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+
+        int[] lines = new int[K];
+        long sum = 0;
+        for (int i = 0; i < K; i++) {
+            int n = Integer.parseInt(br.readLine());
+            lines[i] = n;
+            sum += n;
+        }
+        sum /= N;
+        long left = 1;
+        long right = sum;
+        while (left <= right) {
+            
+            long mid = (left + right) / 2;
+            int cnt = 0;
+            for (int j = 0; j < K; j++) {
+                cnt += lines[j] / mid;
+            }
+            if (cnt >= N) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        System.out.print((left + right) / 2);
+    }
+}
