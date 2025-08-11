7776b9fb577465ffd1fb6ad13d825a813ff07ab1
Taewan Kim <81952405+paladyn12@users.noreply.github.com>
2025-08-11 20:26:25 +0900

    Create Q11286_큐.java

---
+++ src/BAEKJOON/자료구조/큐/Q11286_큐.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Q11286_큐 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> {
+            if (Math.abs(o1) == Math.abs(o2)) return o1 - o2;
+            else return (Math.abs(o1) - Math.abs(o2));
+        });
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num == 0) {
+                if (pq.isEmpty()) {
+                    sb.append("0\n");
+                } else {
+                    sb.append(pq.poll()).append("\n");
+                }
+            } else {
+                pq.offer(num);
+            }
+        }
+        System.out.print(sb);
+    }
+}
