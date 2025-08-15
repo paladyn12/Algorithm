6a17d13fcc1a23a2de8d3ed1410d54e85411a862
Taewan Kim <81952405+paladyn12@users.noreply.github.com>
2025-08-15 10:06:24 +0900

    Create Q10815_집합.java

---
+++ src/BAEKJOON/자료구조/집합/Q10815_집합.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+class Q10815_집합 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        HashSet<Integer> set = new HashSet<>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            set.add(Integer.parseInt(st.nextToken()));
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            sb.append(set.contains(Integer.parseInt(st.nextToken())) ? "1 " : "0 ");
+        }
+        System.out.print(sb);
+    }
+}
